9fa29421d6e3705e67ee68ce7edd697d1f7d40ec
Ayden Kang <kaw0302@163.com>
2021-02-04 00:07:29 +0800

    update

--- js-playground/js-base/1.js
+++ js-playground/js-base/1.js
@@ -1,19 +1,19 @@
-const EventEmitter = require('events');
-
-const req = async () =>
-    new Promise(resolve => {
-        setTimeout(() => {
-            resolve()
-        }, 1000, resolve)
-    })
-
+'use strict';
+const EventEmitter = require('events')
 
 class ConcurrentExecutor {
+
     constructor(concurrency) {
-        this.pool = [];
         this.done = 0;
+        this.pool = [];
         this.working = 0;
         this.concurrency = concurrency;
+        this.event = new EventEmitter();
+        this.event.on('done', () => {
+            this.done++;
+            this.working--;
+            this.run();
+        })
     }
 
     async run() {
@@ -24,8 +24,7 @@ class ConcurrentExecutor {
             const job = this.pool.shift();
             this.working++;
             await job();
-            this.done++;
-            this.working--;
+            this.event.emit('done');
             this.printf();
         }
     }
@@ -33,18 +32,24 @@ class ConcurrentExecutor {
     add(job) {
         this.pool.push(job);
         this.run();
-
     }
 
     printf() {
         console.log(`当前任务池:${this.pool.length}, 最大并发数:${this.concurrency}, 运行中:${this.working}, 已完成:${this.done}`);
     }
+
 }
 
+const concurrentExecutor = new ConcurrentExecutor(4)
 
-const concurrentExecutor = new ConcurrentExecutor(5)
-
-
-for (let i = 0; i < 100; i++) {
-    concurrentExecutor.add(req);
+const reqMock = async () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve()
+        }, Math.random().toFixed(3) * 5000, resolve)
+    })
 }
+
+setInterval(() => {
+    concurrentExecutor.add(reqMock);
+}, 1000)
